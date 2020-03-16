42f5980c63f0f19572f6ab5822de98cc9c1361da
Cholado <sntgmtnz@gmail.com>
2020-03-15 20:37:40 -0500

    started code for the verify_type function

--- _printf.c
+++ _printf.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
-#include <stdarg.h>
-/**
- * main - what do function?
- * @var: function var
- * Return: Always 0
- */
 
-AYAYA Clap
+/**
+ * verify_type - search list of types for a matching format
+ * @format: string of formats from input
+ * Description: refers to struct for valist database of formats.
+ * Return: on success ptr to right function or NULL if failure
+ */
+static int (*verify_type(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
